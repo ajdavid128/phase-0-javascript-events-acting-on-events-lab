4975d0a6dc4ca41b7bdd284bbf02c76d4a2d185a
Aaron David <ajdavid128@gmail.com>
2022-10-12 20:26:10 -0500

    commit tree ac95dae14cab84ba68489d33e35658ec125d71a9

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#cc5a71';
+
+const game = document.getElementById('game');
+game.style.backgroundColor = '#7c7ca2';
+
+// document.addEventListener('keydown', function(e) {
+//     if (e.key === 'ArrowLeft') {
+//         const leftNumber = dodger.style.left.replace('px','');
+//         const left = parseInt(leftNumber, 10);
+
+//         dodger.style.left = `${left - 5}px`;
+//     }
+// });
+
+function moveDodgerLeft() {
+    const leftNumber = dodger.style.left.replace('px','');
+        const left = parseInt(leftNumber, 10);
+
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
+        }
+}
+
+document.addEventListener('keydown', function(e) {
+        if (e.key === 'ArrowLeft') {
+            dodgerMoveLeft();
+        }
+    });
+
+function moveDodgerRight() {
+    const rigthNumber = dodger.style.left.replace('px', '');
+    const right = parseInt(rigthNumber, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+if (e.key === 'ArrowRight') {
+    dodgerMoveRight();
+}
+});
